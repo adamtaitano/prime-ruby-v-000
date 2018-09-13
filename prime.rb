c62d819caf37d223995423e1a0ada8ebef82a8bc
Adam Taitano <adam.taitano@gmail.com>
2018-09-13 06:22:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(integer)
-  limit = integer - Array [2..limit]
+  limit = integer - Array (2..limit)
   if integer_range.each{|number| integer % number == 0}
     false
   else
