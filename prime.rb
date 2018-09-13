9287aefef1fe274fcafa9a3f60f1c12afff64052
Adam Taitano <adam.taitano@gmail.com>
2018-09-13 06:18:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,9 @@
 # Add  code here!
 def prime?(integer)
-  integer_range = [1..integer].to_a
+  integer_range = [2..integer-1].to_a
+  if integer_range.each{|number| integer % number == 0}
+    false
+  else
+    true
+  end
 end
